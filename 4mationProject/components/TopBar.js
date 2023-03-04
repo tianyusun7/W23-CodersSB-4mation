3af283dd93c01ba755c261aeb602278a87e18c2b
shovel-miner <vdhieu01@gmail.com>
2023-03-03 18:42:45 -0800

    Update TopBar.js

--- 4mationProject/components/TopBar.js
+++ 4mationProject/components/TopBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+import SearchBar from './SearchBar.js';
+import JoinedBoardSidePanel from './JoinedBoardSidePanel.js';
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
@@ -12,11 +13,28 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
+  SearchBar: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    flex: 1,
+  },
+  JoinedBoardSidePanel: {
+    flex: 1,
+    justifyingContent: 'left',
+    alignItems: 'left',
+  }
+
 });
+
 
 const TopBar = () => {
   return (
-    <View style={styles.container}>
+    <View style={styles.container} >
+      <SearchBar/>
+      <JoinedBoardSidePanel/>
     </View>
   );
 };
