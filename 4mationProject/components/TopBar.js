61321f31083bb5b7ef1e231ce788d178ce450c42
shovel-miner <vdhieu01@gmail.com>
2023-03-03 18:05:52 -0800

    JoinedBoardSidePanel Button
    add Joined Board Button

---
+++ 4mationProject/components/TopBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import JoinedBoardSidePanel from './JoinedBoardSidePanel';
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      justifyContent: 'left',
+      padding: 10,
+    },
+    text: {
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+    button: {
+        
+    }
+  });
+
