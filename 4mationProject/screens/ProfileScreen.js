ced0f0b160aa7001b2f51de20cebf0bcc9a3f57a
tianyusun7 <tianyusun@ucsb.edu>
2023-03-05 14:01:08 -0800

    update profile screen

--- 4mationProject/screens/ProfileScreen.js
+++ 4mationProject/screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet, Text, Button, TextInput, Alert } from 'react-native';
 
 
 const styles = StyleSheet.create({
@@ -7,12 +7,56 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: `#6495ed`,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
 
-const ProfileScreen = (props) => {
+const ProfileScreen = ({navigation}) => {
+  const [name, onChangeName] = React.useState('');
+
   return (
-    <View style={styles.container}>
-    <Text>This is the profile screen</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style = {styles.title}> {name}'s Profile Screen</Text>
+      <Text>Customize your name Here</Text>
+      <TextInput 
+        style = {styles.input}
+        onChangeText = {name => onChangeName(name)}
+      /> 
+      <Button
+        title="MyBoards"
+        color="#f194ff"
+        onPress={() => Alert.alert('Navigating to boards')}
+      />
+      <Button
+        title="MyPosts/Comments"
+        color="#f194ff"
+        onPress={() => Alert.alert('Navigating to your posts and comments')}
+      />
+      <Button
+        title="Saved"
+        color="#f194ff"
+        onPress={() => Alert.alert('Navigating to saved posts')}
+      />
+      <Button
+        title="Recent"
+        color="#f194ff"
+        onPress={() => Alert.alert('Navigating to recent viewed screen')}
+      />
+      <Button
+        title="Setting"
+        color="#f194ff"
+        onPress={() => Alert.alert('Navigating to setting')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 };
