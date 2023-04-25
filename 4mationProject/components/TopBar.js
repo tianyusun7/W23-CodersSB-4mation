fb8b6888333d544d72a964de40262e50034afe49
hyi96 <hyi@ucsb.edu>
2023-04-24 19:11:54 -0700

    anonymous user directed to sign up page when clicking profile button

--- 4mationProject/components/TopBar.js
+++ 4mationProject/components/TopBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import SearchBar from './SearchBar.js';
 import CustomButton from './CustomButton';
+import { getCurrentUser } from '../firebase.js';
 // displayed in almost every screen
 // TODO: Profile button would direct to the sign up/in page if no current user is signed in
 
@@ -11,7 +12,7 @@ const TopBar = ({ navigation, defaultText }) => {
       <SearchBar defaultText={defaultText}/>
       <CustomButton
         title="Profile"
-        onPress={() => navigation.navigate('Profile')}
+        onPress={() => getCurrentUser()==null ? navigation.navigate('Login') : navigation.navigate('Profile')}
         buttonStyle={{
           backgroundColor: 'white',
           borderColor: 'black',
